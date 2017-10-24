ccccaca5df5df65ebfe82db7db73258d22edc066
Piotr <piotr.ratajczak80@gmail.com>
2017-10-24 17:54:35 +0200

    first JS drum Kit commit

---
+++ main.js
@@ -0,0 +1,22 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+window.addEventListener('keydown', function(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if (!audio) return; //stop the function from running
+    audio.currentTime = 0; //rewind to the start
+    audio.play();
+    key.classList.add('playing');
+});
+
+function removeTransition(e) {
+    if(e.propertyName !== 'transform') return; //skip it if it's not a transform
+    this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
